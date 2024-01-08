ac86bb54cbf286ecbfc8408a9644fe4956888dbe
Pavel Lazureykis <pavel@lazureykis.dev>
2024-01-08 12:35:59 -0500

    Support wildcards in service name

--- src/docker.rs
+++ src/docker.rs
@@ -1,3 +1,4 @@
+use crate::config::Watcher;
 use bollard::service::{InspectServiceOptions, UpdateServiceOptions};
 
 pub async fn get_current_env_vars(service_name: &str) -> crate::result::Result<Vec<String>> {
@@ -64,4 +65,195 @@ pub async fn update_service(
     }
 
     Ok(())
+}
+
+pub fn is_pattern(pattern: &str) -> bool {
+    pattern.contains('*') || pattern.contains('?')
+}
+
+// Matches patters with * and ? wildcards.
+pub fn is_match(string: &str, pattern: &str) -> bool {
+    let m = string.len();
+    let n = pattern.len();
+
+    let mut dp = vec![vec![false; n + 1]; m + 1];
+
+    let sb = string.as_bytes();
+    let pb = pattern.as_bytes();
+
+    dp[m][n] = true;
+
+    for i in (0..=m).rev() {
+        for j in (0..n).rev() {
+            if pb[j] == b'*' {
+                dp[i][j] = dp[i][j + 1];
+
+                if i < m {
+                    dp[i][j] = dp[i][j] || dp[i + 1][j] || dp[i + 1][j + 1];
+                }
+            } else {
+                dp[i][j] = i < m && (sb[i] == pb[j] || pb[j] == b'?') && dp[i + 1][j + 1];
+            }
+        }
+    }
+
+    dp[0][0]
+}
+
+pub async fn list_services(watcher: &Watcher) -> crate::result::Result<Vec<String>> {
+    let docker = bollard::Docker::connect_with_local_defaults()
+        .map_err(|e| format!("Failed to connect to Docker: {}", e))?;
+
+    let docker_services: Vec<_> = docker
+        .list_services(None::<bollard::service::ListServicesOptions<String>>)
+        .await
+        .map_err(|e| format!("Failed to list services: {}", e))?;
+
+    let docker_service_names = docker_services
+        .iter()
+        .filter_map(|service| {
+            let service_name = service.spec.as_ref()?.name.clone()?;
+            Some(service_name)
+        })
+        .collect::<Vec<_>>();
+
+    match_services(watcher, docker_service_names).await
+}
+
+pub async fn match_services(
+    watcher: &Watcher,
+    docker_service_names: Vec<String>,
+) -> crate::result::Result<Vec<String>> {
+    let mut services = vec![];
+
+    for service_name_pattern in &watcher.docker_services {
+        if is_pattern(service_name_pattern) {
+            let mut count = 0;
+            for docker_service_name in &docker_service_names {
+                if !docker_service_name.is_empty()
+                    && is_match(&docker_service_name, service_name_pattern)
+                {
+                    count += 1;
+                    if services.contains(docker_service_name) {
+                        return Err(
+                            "Configuration error: service name cannot be used multiple times"
+                                .into(),
+                        );
+                    } else {
+                        services.push(docker_service_name.to_owned());
+                    }
+                }
+
+                if count == 0 {
+                    return Err(format!(
+                        "Configuration error: no services match pattern {}",
+                        service_name_pattern
+                    )
+                    .into());
+                }
+            }
+        } else {
+            if services.contains(service_name_pattern) {
+                return Err(
+                    "Configuration error: service name cannot be used multiple times".into(),
+                );
+            } else {
+                services.push(service_name_pattern.to_owned());
+            }
+        }
+    }
+
+    Ok(services)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_pattern() {
+        assert!(is_pattern("pattern_with_asterisk*"));
+        assert!(is_pattern("pattern_with_question?"));
+        assert!(is_pattern("complex*?pattern"));
+        assert!(!is_pattern("no_wildcards_pattern"));
+    }
+
+    #[test]
+    fn test_is_match_exact_match() {
+        assert!(is_match("exact_match", "exact_match"));
+    }
+
+    #[test]
+    fn test_is_match_question_start() {
+        assert!(is_match("pattern_start", "??ttern_start"));
+        assert!(!is_match("pattern_start", "?pattern_start"));
+    }
+
+    #[test]
+    fn test_is_match_question_middle() {
+        assert!(is_match("pattern_middle", "pat?ern_middle"));
+        assert!(!is_match("pattern_middle", "pat?tern_middle"));
+    }
+
+    #[test]
+    fn test_is_match_question_end() {
+        assert!(is_match("pattern_end", "pattern_en?"));
+        assert!(!is_match("pattern_end", "pattern_en?d"));
+    }
+
+    #[test]
+    fn test_is_match_star_start() {
+        assert!(is_match("pattern_start", "*ttern_start"));
+        assert!(is_match("pattern_start", "*pattern_start"));
+    }
+
+    #[test]
+    fn test_is_match_star_middle() {
+        assert!(is_match("pattern_middle", "pat*ern_middle"));
+        assert!(is_match("pattern_middle", "pat*tern_middle"));
+    }
+
+    #[test]
+    fn test_is_match_star_end() {
+        assert!(is_match("pattern_end", "pattern_e*"));
+        assert!(is_match("pattern_end", "pattern_en*"));
+    }
+
+    #[tokio::test]
+    async fn test_match_services_exact_match() {
+        let watcher = Watcher {
+            name: "My watcher".to_owned(),
+            docker_services: vec!["service1".to_owned(), "service2".to_owned()],
+            doppler_token: "secret".to_owned(),
+        };
+
+        let docker_service_names = vec!["service1".to_owned(), "service2".to_owned()];
+
+        let result = match_services(&watcher, docker_service_names).await;
+        assert_eq!(
+            result,
+            Ok(vec!["service1".to_owned(), "service2".to_owned()])
+        );
+    }
+
+    #[tokio::test]
+    async fn test_match_services_pattern_and_filtered() {
+        let watcher = Watcher {
+            name: "My watcher".to_owned(),
+            docker_services: vec!["service*".to_owned()],
+            doppler_token: "secret".to_owned(),
+        };
+
+        let docker_service_names = vec![
+            "service1".to_owned(),
+            "service2".to_owned(),
+            "another_service".to_owned(),
+        ];
+
+        let result = match_services(&watcher, docker_service_names).await;
+        assert_eq!(
+            result,
+            Ok(vec!["service1".to_owned(), "service2".to_owned()])
+        );
+    }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,6 @@
+#[derive(Clone, PartialEq, Eq)]
 pub struct Error(String);
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.0)
